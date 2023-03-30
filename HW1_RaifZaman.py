820b006a2fca0c05fd177f9cef67c06c9748aa23
raifzaman <128110040+raifzaman@users.noreply.github.com>
2023-03-30 14:14:52 -0400

    Update HW1_RaifZaman.py

--- HW1_RaifZaman.py
+++ HW1_RaifZaman.py
@@ -126,14 +126,14 @@ TotalCost = Costof60books + ShippingforBookNum1 + ShippingforRestofBooks
 print("The total wholesale cost for 60 books is: ", TotalCost)
 3. 
 start_time = (6*60 + 52)*60
-easy_time = (8*60 + 15)*2
-tempo_time = (7*60 + 12)*3
+easyPace = (8*60 + 15)*2
+tempoPace = (7*60 + 12)*3
 
-breakfast_hour = (start_time + easy_time + tempo_time)/(60*60)
+breakfast_hour = (start_time + easyPace + tempoPace)/(60*60)
 breakfast_int_hour = int(breakfast_hour)
 
 breakfast_minute  = (breakfast_hour - breakfast_int_hour)*60
 breakfast_int_minute = int(breakfast_minute)
 
-print('Breakfast is at {}.{}'.format(breakfast_int_hour,breakfast_int_minute))
+print("Breakfast is at", breakfast_int_hour,":",breakfast_int_minute)
 '''
